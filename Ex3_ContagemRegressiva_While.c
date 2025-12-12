95c886a63f8ca767ae7f066ce3158d58a34f7b81
wSantosInfoSec <welberferreira2014@gmail.com>
2025-12-12 14:08:39 -0300

    Add files via upload

---
+++ Ex3_ContagemRegressiva_While.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+int nUser;
+
+printf ("Escolha um número para a contagem regressiva:" );
+
+scanf ("%d", &nUser);
+	
+	
+	while (nUser>=0) {
+		printf ("%d\n", nUser);
+	nUser--;}
+}
+RETURN 0
